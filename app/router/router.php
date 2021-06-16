82f371439e124eca5224b8423a7b906473964a0f
Emilie <Emilie@DESKTOP-QAIGEF2>
2021-06-16 12:20:20 +0200

    arborescence

---
+++ app/router/router.php
@@ -0,0 +1,54 @@
+
+<!-- ----- debut Router -->
+<?php
+require ('../controller/ControllerVaccin.php');
+require ('../controller/ControllerCentre.php');
+require ('../controller/ControllerCovid.php');
+
+// --- récupération de l'action passée dans l'URL
+$query_string = $_SERVER['QUERY_STRING'];
+
+// fonction parse_str permet de construire 
+// une table de hachage (clé + valeur)
+parse_str($query_string, $param);
+
+// --- $action contient le nom de la méthode statique recherchée
+$action = htmlspecialchars($param["action"]);
+
+// --- On supprime l'élément action de la structure
+unset($param['action']);
+
+// --- Tout ce qui reste sont des arguments
+$args = $param;
+
+// --- Liste des méthodes autorisées
+switch ($action) {
+    case "VaccinReadAll" :
+    case "VaccinReadOne" :
+    case "VaccinReadId" :
+    case "VaccinCreate" :
+    case "VaccinCreated" :
+        // --- passage des arguments au contrôleur
+        ControllerVaccin::$action($args);
+        break;
+
+    case "CentreReadAll" :
+    case "CentreReadOne" :
+    case "CentreReadId" :
+    case "CentreCreate" :
+    case "CentreCreated" :
+        ControllerCentre::$action($args);
+        break;
+
+    case "#" :
+        ControllerCovid::$action($args);
+        break;
+
+// Tache par défaut
+    default:
+        $action = "covidAccueil";
+        ControllerCovid::$action($args);
+}
+?>
+<!-- ----- Fin Router -->
+
---
+++ app/view/viewCovidAccueil.php
@@ -0,0 +1,20 @@
+ 
+<!-- ----- debut de la page d'accueil -->
+<?php include 'fragment/fragmentCovidHeader.html'; ?>
+<body>
+  <div class="container">
+    <?php
+    include 'fragment/fragmentCovidMenu.html';
+    include 'fragment/fragmentCovidJumbotron.html';
+    ?>
+  </div>   
+  
+  
+  <?php
+  include 'fragment/fragmentCovidFooter.html';
+  ?>
+
+  <!-- ----- fin de la page -->
+
+</body>
+</html>
