6ae843b4beead37d06b875d7ce556039aa2b0fd8
Emilie <Emilie@DESKTOP-QAIGEF2>
2021-06-18 22:33:31 +0200

    gestion patient

--- app/view/patient/viewInsert.php
+++ app/view/patient/viewInsert.php
@@ -15,9 +15,9 @@ require ($root . '/app/view/fragment/fragmentCovidHeader.html');
         <form role="form" method='get' action='router.php'>
             <div class="form-group">
                 <input type="hidden" name='action' value='PatientCreated'>        
-                <label for="id">nom : </label><input type="text" name='nom' size='50' value='Pierre'>
-                <label for="id">prenom : </label><input type="text" name='prenom' size='50' value='Paul'>   
-                <label for="id">adresse : </label><input type="text" name='adresse' size='50' value='Rue des Gayettes'>              
+                <label for="id">nom : </label><input type="text" name='nom' size='25' value='Pierre'>
+                <label for="id">prenom : </label><input type="text" name='prenom' size='25' value='Paul'>  
+                <label for="id">adresse : </label><input type="text" name='adresse' size='50' value='Bordeaux'>              
             </div>
             <p/>
             <button class="btn btn-primary" type="submit">Go</button>
